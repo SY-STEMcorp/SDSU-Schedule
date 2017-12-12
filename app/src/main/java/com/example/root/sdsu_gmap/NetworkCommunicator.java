9976f71878879783a00c4c200e195ec573e605f4
levsana <levanisana8@gmail.com>
2017-12-12 15:59:25 +0400

    Converting HTTPUrlConnection requests from GET to POST

--- app/src/main/java/com/example/root/sdsu_gmap/NetworkCommunicator.java
+++ app/src/main/java/com/example/root/sdsu_gmap/NetworkCommunicator.java
@@ -1,14 +1,13 @@
 package com.example.root.sdsu_gmap;
 
-import android.content.Intent;
 import android.os.AsyncTask;
-import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by root on 11/17/17.
@@ -17,6 +16,7 @@ import java.net.URL;
 public class NetworkCommunicator extends AsyncTask<Void, Void, String> {
 
     private String url = "";
+    private String urlParameters = "";
 
     NetworkCommunicator(String url) {
         this.url = url;
@@ -26,9 +26,19 @@ public class NetworkCommunicator extends AsyncTask<Void, Void, String> {
     protected String doInBackground(Void... params) {
 
         try {
+
+            byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+
             URL LoginURL = new URL(url);
             //TODO: Convert this to https (HttpsURLConnection)
             HttpURLConnection conn = (HttpURLConnection) LoginURL.openConnection();
+            conn.setDoOutput(true);
+            conn.setInstanceFollowRedirects(false);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("charset", "utf-8");
+            conn.setRequestProperty("Content-Length", Integer.toString(postData.length));
 
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
